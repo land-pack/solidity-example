d0230bcc8e96fa580c9e7d5e1c34867462ef636e
land-pack <landpack@sina.com>
2018-01-25 15:03:45 +0800

    add hello

---
+++ hello.js
@@ -0,0 +1,18 @@
+pagma solidity ^0.4.0;
+contract Person{
+    
+    string name;
+    
+    function Person() public{
+        name = "Frank AK";
+    }
+    
+    function sayHello() public constant returns (string, string){
+        return ("Hello", name);
+    }
+    
+    function setName(string n) public{
+        name = n;
+    }
+
+}
