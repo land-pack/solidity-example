97aad1ce343bfeee79503884ab1121e615d62dd7
land-pack <landpack@sina.com>
2018-01-29 11:43:06 +0800

    update

---
+++ truffle_command/README.md
@@ -0,0 +1,7 @@
+# Input & Output command
+
+    truffle(development)> hw.balance.call();;
+    BigNumber { s: -1, e: 0, c: [ 1 ] }
+    # s --> is digit symbol
+    # e --> is exponent
+    # c --> is number
--- truffle_project/hello/contracts/HelloWorld.sol
+++ truffle_project/hello/contracts/HelloWorld.sol
@@ -2,9 +2,30 @@ pragma solidity ^0.4.18;
 
 contract HelloWorld {
     uint public balance;
+    int public balance2;
+    bool public flag;
+
+    mapping(uint => uint) public names;
+    string public ower;
     
     // constructor
-    function HelloWorld() {
+    function HelloWorld(uint _balance) {
         balance = 0;
+        balance2 = -1;
+        ower = "Frank";
+        flag = true;
+
+    }
+
+    function update_balance(uint _balance) {
+        balance = _balance;
+    }
+
+    function update_balance2(int _balance) {
+        balance2 = _balance;
+    }
+
+    function update_names(uint _id, uint _age) {
+        names[_id] = _age;
     }
 }
